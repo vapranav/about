356fd511497c085d5aeeaa54983a60f18fb93810
vapranav <pranavva34@gmail.com>
2020-08-25 15:55:05 +0530

    Updates

---
+++ precache-manifest.5a930d5231bd988d2262ca5a4f633d6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "344633873786c32f58a660c491dcb25f",
+    "url": "/about/index.html"
+  },
+  {
+    "revision": "d466fb1ce2c71229ad29",
+    "url": "/about/static/css/main.84af885b.chunk.css"
+  },
+  {
+    "revision": "cebe90d170efc76772af",
+    "url": "/about/static/js/2.b644ef17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/about/static/js/2.b644ef17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d466fb1ce2c71229ad29",
+    "url": "/about/static/js/main.e643f88f.chunk.js"
+  },
+  {
+    "revision": "f7bcb19dcb4db879a129",
+    "url": "/about/static/js/runtime-main.7b24b706.js"
+  }
+]);
