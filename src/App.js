3ab3fbd1ea0322c0a81c3530ac62a01ab940ae45
vapranav <pranavva34@gmail.com>
2020-08-25 08:59:12 +0530

    add react-reveal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import Fade from 'react-reveal/Fade';
 
 import "./App.css";
 
 function App() {
 
   return (
-    <div className="App">
-      <div className="title" > Not a portfolio 😋 </div>
-      <h1><span className="highlight">Currently</span></h1>
-      <div>
-      <ul>Grinding Leetcode</ul>
-      <ul><b>Groovr</b> - Meet people based on similar music taste. </ul>
+    <Fade bottom>
+        <div className="App">
+        <div className="title" > Not a portfolio 😋 </div>
+        <h1><span className="highlight">Currently</span></h1>
+        <div>
+        <ul>Grinding Leetcode</ul>
+        <ul><b>Groovr</b> - Meet people based on similar music taste. </ul>
+        </div>
+        <h1><span className="highlight">Worked on</span></h1>
+        <ul>Listify</ul>
+        <ul>Discord Football Bot</ul>
+        <h1>I am </h1>
       </div>
-      <h1><span className="highlight">Worked on</span></h1>
-      <ul>Listify</ul>
-      <ul>Discord Football Bot</ul>
-      <h1>I am </h1>
-    </div>
+    </Fade>
+    
   );
 }
 
